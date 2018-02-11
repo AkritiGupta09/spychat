d24ff7ed62bc05f2bf729c47d4c2400d2ec416d4
Akriti <akritigupta65@gmail.com>
2018-02-11 22:59:55 +0530

    module 3 & 4 complete

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-from spy_details import spy_salutation,spy_name,spy_age,spy_rating
+from spy_details import spy
 print ("Welcome to spychat!! let's get started")
 
+NEW_USER_DEATAILS=[]
 STATUS_MSG_LIST=['On Mission','JAMES BOND','SHERLOCK HOLMES','DETECTING','Spychat only']#creation of list containing status
-FRIEND_NAME=[]
-FRIEND_AGE=[]
-FRIEND_RATING=[]
-FRIEND_ONLINE=[]
+FRIEND_DETAILS=[]
 
 def add_status(current_status_msg):#func creation for adding status
     if current_status_msg == None:
@@ -21,7 +19,7 @@ def add_status(current_status_msg):#func creation for adding status
             print str(sr_no)+"."+ all_status
             sr_no=sr_no+1
         choose_status = input("From the given choices,which one do you want to put now?")
-        put_status=STATUS_MSG_LIST(choose_status-1)
+        put_status=STATUS_MSG_LIST[choose_status-1]
 
 
     elif user_status_choice.upper() =='N':
@@ -33,23 +31,24 @@ def add_status(current_status_msg):#func creation for adding status
 
 
 def add_friend():#func creation for adding frnd
-    frnd_name=raw_input("What is your name?")#asking for details n appending them
-    frnd_salutation=raw_input("What should I call you? mr ms dr er")
-    frnd_salutation=frnd_salutation.upper()
-    frnd_name=frnd_salutation+"."+frnd_name
-    frnd_age=input("What is you age?")
-    frnd_rating=input("Tell me your rating")
-    frnd_online=True
 
-    if len(frnd_name)>2 and 50>=frnd_age>=12 and frnd_rating>=spy_rating:
-        FRIEND_NAME.append(frnd_name)
-        FRIEND_AGE.append(frnd_age)
-        FRIEND_RATING.append(frnd_rating)
-        FRIEND_ONLINE.append(True)
+    frnd = {'salutation': '', 'name': '', 'age': 0, 'rating': 0.0, 'online': 'True'}  # dictionary creation
+
+    frnd['name']=raw_input("What is your name?")#asking for details n appending them
+    frnd['salutation']=raw_input("What should I call you? mr ms dr er")
+    frnd['salutation']=frnd['salutation'].upper()
+    frnd['name']=frnd['salutation']+"."+frnd['name']
+    frnd['age']=input("What is you age?")
+    frnd['rating']=input("Tell me your rating")
+    frnd['online']=True
+
+    if len(frnd['name'])>2 and 50>=frnd['age']>=12 and frnd['rating']>=spy['rating']:
+        FRIEND_DETAILS.append(frnd)
+
     else:
         print"The details entered do not meet the requirement to be your friend"
 
-    return len(FRIEND_NAME)
+    return len(FRIEND_DETAILS)
 
 
 
@@ -58,29 +57,32 @@ def spychat(spy_salutation,spy_name,spy_age,spy_rating):#func creation
 
     menu_choice_defaultuser = raw_input("Do you want to continue as default user?Y N")  # menuchoice creation for dfault user
     if menu_choice_defaultuser.upper()=='Y':
-        print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f" % (spy_salutation, spy_name, spy_age, spy_rating))
+        print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f" % (spy_salutation,spy_name,spy_age,spy_rating))
 
     elif menu_choice_defaultuser.upper()=='N':
         print "Let's create a new user"#creation of new user
-        user1_name=raw_input("What is your name?")
-        user1_age=input("Enter your age")
-        if user1_age>12 and user1_age<50:
+
+        new_user={'username':'','userage':0,'userrating':0.0}#dic for newuser
+
+        new_user['username']=raw_input("What is your name?")
+        new_user['userage']=input("Enter your age")
+        if new_user['userage']>12 and new_user['userage']<50:
             print"You are of appropriate age."#for new user's age
-            user1_rating=input("Tell us your rating")
-            if spy_rating==10.0:
+            new_user['userrating']=input("Tell us your rating")
+            if new_user['userrating']==10.0:
                 print("You seem to be very intelligent")
-            elif spy_rating>=8.0:
+            elif new_user['userrating']>=8.0:
                 print("You seem to be a good spy")
-            elif spy_rating>=5:
+            elif new_user['userrating']>=5:
                 print("You seem to be more than average spy")
-            elif spy_rating<5 and spy_rating>=4:
+            elif new_user['userrating']<5 and new_user['userrating']>=4:
                 print("You seem to be an average spy")
-            elif spy_rating==3:
+            elif new_user['userrating']==3:
                 print("You seem to be below average spy")
             else:
                 print("You really need to improve")
 
-            print  ("Your details are:\n Name= %s Age:%d Rating:%.3f"% (user1_name,user1_age,user1_rating))
+            print  ("Your details are:\n Name= %s Age:%d Rating:%.3f"% (new_user['username'],new_user['userage'],new_user['userrating']))
         else:
             print ("We are sorry but you are retired now")#for new user
 
@@ -111,38 +113,39 @@ def spychat(spy_salutation,spy_name,spy_age,spy_rating):#func creation
 ask_user=raw_input("Are you a current user? Y  N")#asking for default user or new one
 if ask_user.upper() == 'Y':
     print("We already have your details as you are a current user. ")
-    spychat(spy_salutation,spy_name,spy_age,spy_rating) #func called
+    spychat(spy['salutation'],spy['name'],spy['age'],spy['rating']) #func called
 
 
 elif ask_user.upper()=='N':
 
-    spy_name=raw_input("what is your name?")#asking for details
-    if len(spy_name)>=4:
-        print("Hi! "+spy_name)
+    spy = {'salutation': '', 'name': '', 'age': 0, 'rating': 0.0}  # dictionary creation
 
-        spy_salutation=raw_input("Fill your salutation:mr ms dr er")
-        spy_salutation=spy_salutation.upper()
-        spy_age=0
-        spy_age=input("How old are you?")
-        if spy_age>12 and spy_age<50:
+    spy['name']=raw_input("what is your name?")#asking for details
+    if len(spy['name'])>=4:
+        print("Hi! "+spy['name'])
+
+        spy['salutation']=raw_input("Fill your salutation:mr ms dr er")
+        spy['salutation']=spy['salutation'].upper()
+
+        spy['age']=input("How old are you?")
+        if spy['age']>12 and spy['age']<50:
             print "You are of appropriate age"
 
-            spy_rating=0.0
-            spy_rating=input("Tell me your rating")
-            if spy_rating==10.0:
+            spy['rating']=input("Tell me your rating")
+            if spy['rating']==10.0:
                 print("You seem to be very intelligent")
-            elif spy_rating>=8.0:
+            elif spy['rating']>=8.0:
                 print("You seem to be a good spy")
-            elif spy_rating>=5:
+            elif spy['rating']>=5:
                 print("You seem to be more than average spy")
-            elif spy_rating<5 and spy_rating>=4:
+            elif spy['rating']<5 and spy['rating']>=4:
                 print("You seem to be an average spy")
-            elif spy_rating==3:
+            elif spy['rating']==3:
                 print("You seem to be below average spy")
             else:
                 print("You really need to improve")
-            print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f"% (spy_salutation,spy_name,spy_age,spy_rating))#final print of details
-            spychat(spy_salutation, spy_name, spy_age, spy_rating)
+            print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f"% (spy['salutation'],spy['name'],spy['age'],spy['rating']))#final print of details
+            spychat(spy['salutation'], spy['name'], spy['age'], spy['rating'])
         else:
             print("You are retired now as your age is invalid")
     else:
--- spy_details.py
+++ spy_details.py
@@ -1,5 +1,2 @@
-spy_salutation='ms'
-spy_name = 'akriti'
-spy_age = 21
-spy_rating = 2.1
-spy_surname = 'gupta'
+
+spy={'salutation':'ms','name':'akriti','age':21,'rating':2.1,'surname':'gupta'}#dic creation
