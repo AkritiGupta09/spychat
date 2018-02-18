3b0ea686d627440648d40de6274965a22761b50a
Akriti <akritigupta65@gmail.com>
2018-02-18 18:39:40 +0530

    spyclass created,csv n loading of files done

--- main.py
+++ main.py
@@ -1,50 +1,81 @@
-from spy_details import spy
+from spy_details import spy,FRIEND_DETAILS
+from spy_details import Spy,Chatmsgs,Newuser,Friend
 from steganography.steganography import Steganography
 from datetime import datetime
+import time
 import csv
 
 
 print ("Welcome to spychat!! let's get started")
 
-NEW_USER_DEATAILS=[]
 STATUS_MSG_LIST=['On Mission','JAMES BOND','SHERLOCK HOLMES','DETECTING','Spychat only']#creation of list containing status
-FRIEND_DETAILS=[{'name':'shefali','age':21,'rating':5.6,'online':True,'chats':[]},{'name':'shivani','age':22,'rating':4.5,'online':True,'chats':[]}]
 
+def load_friends():
+    with open('friends1.csv','rb') as friends_data:
+        reader = list(csv.reader(friends_data))
 
+        for row in reader[1:]:
+            if row:
+                name=row[0]
+                salutation=row[1]
+                age=row[2]
+                rating=[3]
+                spy=Spy(name,salutation,age,rating)
+                FRIEND_DETAILS.append(spy)
+load_friends()
+
+def load_chats():
+    with open('chats.csv', 'rb') as chats_data:
+        reader = list(csv.reader(chats_data))
+
+        for row in reader[1:]:
+            if row:
+                secret_msg=row[0]
+                time=row[1]
+                sender_you=row[2]
+                print "Chats are:"+secret_msg+" "+time+" "+sender_you
+load_chats()
 
 def select_a_frnd():#func creation to select a frnd
     sr_no=1
     for frnd in FRIEND_DETAILS:
-        print str(sr_no)+"."+frnd['name']
+        print str(sr_no)+"."+frnd.name
         sr_no=sr_no+1
 
     choose_frnd=input("Choose a friend:")
-    actual_frnd=FRIEND_DETAILS[choose_frnd-1]
-    print "The message will be sent to" +str(actual_frnd)
+    actual_frnd=choose_frnd-1
 
     return actual_frnd
 
 def send_a_msg():#func created to send a msg
     chosen_frnd_to_send=select_a_frnd()
+    print "The msg will be sent to:" +" " +FRIEND_DETAILS[chosen_frnd_to_send].name
     original_img=raw_input("Write a name of the image with which you to encode the image:")
     secret_msg=raw_input("Write the message which you want to encrypt:")
     output_path="OUTPUT.jpeg"
     Steganography.encode(original_img,output_path,secret_msg)
+    time=datetime.now()
+    sender_you=True
 
-    user_chat={'message':secret_msg,'time_of_msg':datetime.now(),'sender_you':True}#dic creation for timestamp
-    print "MESSAGE IS ENCRYPTED!"
+    user_chat=Chatmsgs(secret_msg,time,sender_you)#objct creation
+    print "MESSAGE IS ENCRYPTED!"+"at"+time.strftime("%a ,%d %m %Y, %H:%M")
+    FRIEND_DETAILS[chosen_frnd_to_send].chats.append(user_chat)
 
-    FRIEND_DETAILS[chosen_frnd_to_send]['chats'].append(user_chat)
+    with open('chats.csv', 'a') as chats_data:#save to csv
+        writer = csv.writer(chats_data)
+        writer.writerow([FRIEND_DETAILS[chosen_frnd_to_send],secret_msg,time])
+
 
 def read_a_msg():#funccreated to send a msg
     chosen_frnd_to_read=select_a_frnd()
+    print "You are reading from "+" "+ FRIEND_DETAILS[chosen_frnd_to_read].name
     output_img_name=raw_input("Select the image from which data is to be decoded:")
     decrypted_text=Steganography.decode(output_img_name)
-
-    user_chat = {'message': decrypted_text, 'time_of_msg': datetime.now(), 'sender_you': False}  # dic creation for timestamp
-    FRIEND_DETAILS[chosen_frnd_to_read]['chats'].append(user_chat)
-
+    time=datetime.now()
+    sender_you=False
+    user_chat=Chatmsgs(decrypted_text,time,sender_you)#objctcreation
     print 'Your decoded message is:' + decrypted_text
+    FRIEND_DETAILS[chosen_frnd_to_read].chats.append(user_chat)
 
 
 def add_status(current_status_msg):#func creation for adding status
@@ -74,22 +105,21 @@ def add_status(current_status_msg):#func creation for adding status
 
 def add_friend():#func creation for adding frnd
 
-    frnd = {'salutation': '', 'name': '', 'age': 0, 'rating': 0.0, 'online': 'True'}  # dictionary creation
+    frnd=Friend('','',0,0.0)#object creation
 
-    frnd['name']=raw_input("What is your name?")#asking for details n appending them
-    frnd['salutation']=raw_input("What should I call you? mr ms dr er")
-    frnd['salutation']=frnd['salutation'].upper()
-    frnd['name']=frnd['salutation']+"."+frnd['name']
-    frnd['age']=input("What is you age?")
-    frnd['rating']=input("Tell me your rating")
-    frnd['online']=True
+    frnd_name=raw_input("What is your name?")#asking for details n appending them
+    frnd_salutation=raw_input("What should I call you? mr ms dr er")
+    frnd_salutation=frnd.salutation.upper()
+    frnd_name=frnd.salutation+"."+" "+frnd.name
+    frnd_age=input("What is you age?")
+    frnd_rating=input("Tell me your rating")
+    frnd_online=True
 
-    if len(frnd['name'])>2 and 50>=frnd['age']>=12 and frnd['rating']>=spy['rating']:
+    if len(frnd_name)>2 and 50>=frnd_age>=12 and frnd_rating>=spy.rating:
         FRIEND_DETAILS.append(frnd)
-        with open('friends.csv', 'a') as friend_data:
-            writer=csv.writer(friend_data)
-            writer.writerow(frnd['salutation'],frnd['name'],frnd['age'],frnd['rating'],frnd['online'])
-
+        with open('friends1.csv', 'a') as friends_data:
+            writer=csv.writer(friends_data)
+            writer.writerow([frnd.name,frnd.age,frnd.rating,frnd.online])
     else:
         print"The details entered do not meet the requirement to be your friend"
 
@@ -102,32 +132,34 @@ def spychat(spy_salutation,spy_name,spy_age,spy_rating):#func creation
 
     menu_choice_defaultuser = raw_input("Do you want to continue as default user?Y N")  # menuchoice creation for dfault user
     if menu_choice_defaultuser.upper()=='Y':
-        print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f" % (spy_salutation,spy_name,spy_age,spy_rating))
+        print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f" % (spy.salutation,spy.name,spy.age,spy.rating))
 
     elif menu_choice_defaultuser.upper()=='N':
         print "Let's create a new user"#creation of new user
 
-        new_user={'username':'','userage':0,'userrating':0.0}#dic for newuser
+        new_user=Newuser('',0,0.0)#object created for class Newuser
 
-        new_user['username']=raw_input("What is your name?")
-        new_user['userage']=input("Enter your age")
-        if new_user['userage']>12 and new_user['userage']<50:
+        new_user.newuser_name=raw_input("What is your name?")
+
+        new_user.newuser_age=input("Enter your age")
+        if new_user.newuser_age>12 and new_user.newuser_age<50:
             print"You are of appropriate age."#for new user's age
-            new_user['userrating']=input("Tell us your rating")
-            if new_user['userrating']==10.0:
+
+            new_user.newuser_rating=input("Tell us your rating")
+            if new_user.newuser_rating==10.0:
                 print("You seem to be very intelligent")
-            elif new_user['userrating']>=8.0:
+            elif new_user.newuser_rating>=8.0:
                 print("You seem to be a good spy")
-            elif new_user['userrating']>=5:
+            elif new_user.newuser_rating>=5:
                 print("You seem to be more than average spy")
-            elif new_user['userrating']<5 and new_user['userrating']>=4:
+            elif new_user.newuser_rating<5 and new_user.newuser_rating>=4:
                 print("You seem to be an average spy")
-            elif new_user['userrating']==3:
+            elif new_user.newuser_rating==3:
                 print("You seem to be below average spy")
             else:
                 print("You really need to improve")
 
-            print  ("Your details are:\n Name= %s Age:%d Rating:%.3f"% (new_user['username'],new_user['userage'],new_user['userrating']))
+            print  ("Your details are:\n Name= %s Age:%d Rating:%.3f"% (new_user.newuser_name,new_user.newuser_age,new_user.newuser_rating))
         else:
             print ("We are sorry but you are retired now")#for new user
 
@@ -136,8 +168,7 @@ def spychat(spy_salutation,spy_name,spy_age,spy_rating):#func creation
 
     show_menu = True #menucoice creation for features
     while show_menu:
-        menu_choice_for_features = input(
-            "Which feature do you want to explore?\n 1.Add a status update\n 2.Add a friend\n 3.Send a secret message\n 4.Read a secret message\n 5.Read chats from a user\n 6.Close application\n")
+        menu_choice_for_features = input("Which feature do you want to explore?\n 1.Add a status update\n 2.Add a friend\n 3.Send a secret message\n 4.Read a secret message\n 5.Read chats from a user\n 6.Close application\n")
         if menu_choice_for_features == 1:
             updated_status_msg = add_status(current_status_msg)#addstatus func call
             print "YOUR STATUS:" + updated_status_msg
@@ -158,39 +189,40 @@ def spychat(spy_salutation,spy_name,spy_age,spy_rating):#func creation
 ask_user=raw_input("Are you a current user? Y  N")#asking for default user or new one
 if ask_user.upper() == 'Y':
     print("We already have your details as you are a current user. ")
-    spychat(spy['salutation'],spy['name'],spy['age'],spy['rating']) #func called
+    spychat(spy.salutation,spy.name,spy.age,spy.rating) #func called
 
 
 elif ask_user.upper()=='N':
 
-    spy = {'salutation': '', 'name': '', 'age': 0, 'rating': 0.0}  # dictionary creation
+    spy=Spy('','',0,0.0)#object creation named spy for Spy class
 
-    spy['name']=raw_input("what is your name?")#asking for details
-    if len(spy['name'])>=4:
-        print("Hi! "+spy['name'])
+    spy.name=raw_input("what is your name?")#asking for details
+    if len(spy.name)>=4:
+        print("Hi! "+spy.name)
 
-        spy['salutation']=raw_input("Fill your salutation:mr ms dr er")
-        spy['salutation']=spy['salutation'].upper()
+        spy.salutation=raw_input("Fill your salutation:mr ms dr er")
+        spy.salutation=spy.salutation.upper()
 
-        spy['age']=input("How old are you?")
-        if spy['age']>12 and spy['age']<50:
+        spy.online=True
+        spy.age=input("How old are you?")
+        if spy.age>12 and spy.age<50:
             print "You are of appropriate age"
 
-            spy['rating']=input("Tell me your rating")
-            if spy['rating']==10.0:
+            spy.rating=input("Tell me your rating")
+            if spy.rating==10.0:
                 print("You seem to be very intelligent")
-            elif spy['rating']>=8.0:
+            elif spy.rating>=8.0:
                 print("You seem to be a good spy")
-            elif spy['rating']>=5:
+            elif spy.rating>=5:
                 print("You seem to be more than average spy")
-            elif spy['rating']<5 and spy['rating']>=4:
+            elif spy.rating<5 and spy.rating>=4:
                 print("You seem to be an average spy")
-            elif spy['rating']==3:
+            elif spy.rating==3:
                 print("You seem to be below average spy")
             else:
                 print("You really need to improve")
-            print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f"% (spy['salutation'],spy['name'],spy['age'],spy['rating']))#final print of details
-            spychat(spy['salutation'], spy['name'], spy['age'], spy['rating'])#func call
+            print("Your details are:\n Name= %s.%s Age:%d Rating:%.3f"% (spy.salutation,spy.name,spy.age,spy.rating))#final print of details
+            spychat(spy.salutation, spy.name, spy.age, spy.rating)#func call
         else:
             print("You are retired now as your age is invalid")
     else:
--- spy_details.py
+++ spy_details.py
@@ -1,2 +1,40 @@
+from datetime import datetime
 
-spy={'salutation':'ms','name':'akriti','age':21,'rating':2.1,'surname':'gupta'}#dic creation
+class Spy:
+    def __init__(self,name,salutation,age,rating):
+        self.name=name
+        self.salutation=salutation
+        self.age=age
+        self.rating=rating
+        self.online=True
+        self.chats=[]
+        self.current_status_msg=None
+
+spy=Spy("akriti","ms",22,0.1)
+
+friend_one=Spy("shefali",'ms',20,3.4)
+friend_two=Spy("shivani",'ms',22,3.2)
+friend_three=Spy("arshia",'ms',25,5.4)
+
+FRIEND_DETAILS=[friend_one,friend_two,friend_three]
+
+
+class Newuser:
+    def __init__(self,newuser_name,newuser_age,newuser_rating):
+        self.newuser_name=newuser_name
+        self.newuser_age=newuser_age
+        self.newuser_rating=newuser_rating
+
+class Friend:
+    def __init__(self,salutation,name,age,rating):
+        self.salutation=salutation
+        self.name=name
+        self.age=age
+        self.rating=rating
+
+
+class Chatmsgs:
+    def __init__(self,secret_msg,time_of_msg,sender_you):
+        self.secret_msg=secret_msg
+        self.time_of_msg=time_of_msg
+        self.sender_you=sender_you
